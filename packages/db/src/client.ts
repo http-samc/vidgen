d03ed0887497af792ae6a0b1930ea729770521f4
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2025-06-11 13:52:33 -0700

    Migrate DB

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,9 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle({
-  client: sql,
+export const db = drizzle(process.env.POSTGRES_URL!, {
   schema,
   casing: "snake_case",
 });
