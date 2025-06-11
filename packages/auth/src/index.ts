474ee0a7a0536a85986828dfa95312adc7ce5534
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2025-06-11 13:21:54 -0700

    t3 cleanup

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,8 +1,6 @@
 import type { BetterAuthOptions } from "better-auth";
-import { expo } from "@better-auth/expo";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { oAuthProxy } from "better-auth/plugins";
 
 import { db } from "@acme/db/client";
 
@@ -10,9 +8,6 @@ export function initAuth(options: {
   baseUrl: string;
   productionUrl: string;
   secret: string | undefined;
-
-  discordClientId: string;
-  discordClientSecret: string;
 }) {
   const config = {
     database: drizzleAdapter(db, {
@@ -20,24 +15,8 @@ export function initAuth(options: {
     }),
     baseURL: options.baseUrl,
     secret: options.secret,
-    plugins: [
-      oAuthProxy({
-        /**
-         * Auto-inference blocked by https://github.com/better-auth/better-auth/pull/2891
-         */
-        currentURL: options.baseUrl,
-        productionURL: options.productionUrl,
-      }),
-      expo(),
-    ],
     socialProviders: {
-      discord: {
-        clientId: options.discordClientId,
-        clientSecret: options.discordClientSecret,
-        redirectURI: `${options.productionUrl}/api/auth/callback/discord`,
-      },
     },
-    trustedOrigins: ["expo://"],
   } satisfies BetterAuthOptions;
 
   return betterAuth(config);
