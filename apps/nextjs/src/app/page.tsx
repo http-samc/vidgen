2086541d271c73429db84db3cd25f385027aeba4
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2025-06-13 00:54:07 -0700

    Update page.tsx

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -5,7 +5,7 @@ import AuthButton from "~/components/auth-button";
 export default function HomePage() {
   return (
     <main className="grid h-screen w-full place-content-center gap-4">
-      <div className="space-y-2">
+      <div className="w-64 space-y-2">
         <h1 className="text-4xl font-bold">VidGen</h1>
         <p className="text-lg text-muted-foreground">
           AI generated short-form videos by{" "}
@@ -16,6 +16,7 @@ export default function HomePage() {
           >
             smrth
           </Link>
+          .
         </p>
       </div>
       <AuthButton />
